857c5b69d61113f9ce8365412279bd06b81a335b
Rafael Mih <rafaelwsantos@gmail.com>
2023-05-17 22:07:56 -0300

    teste

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as dotenv from 'dotenv';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as dotenv from "dotenv";
 dotenv.config();
-ReactDOM.render(
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
