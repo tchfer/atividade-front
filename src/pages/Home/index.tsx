809a43873b6cdedf71c57198e21100cb077bc522
tchfer <tchfernando@gmail.com>
2023-05-31 03:05:47 -0300

    Revert "Adjust home page styling"
    This reverts commit ba5ae02f0319c91d7469e69d6027f0611eb81425.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,35 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Lottie, { LottieProps } from 'react-lottie';
-import animationData from './animation.json';
-import './home.css';
-
-const lottieOptions: LottieProps['options'] = {
-  loop: true,
-  autoplay: true,
-  animationData: animationData,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice'
-  }
-};
-
+ 
 const Home: React.FC = () => {
-  return (
-    <div className="home-container">
-      <div className="content">
-        <div className="image-container">
-          <Lottie options={lottieOptions} />
-        </div>
-        <div className="text-container">
-          <h1 className="home-title">Meu Gerenciador de Tarefas</h1>
-          <p className="home-description">Organize suas tarefas de forma simples e eficiente.</p>
-          <Link to="/tarefas" className="task-button">
-            Começar
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
-
+    return <h1>Página inicial</h1>
+}
+ 
 export default Home;
