8cd547a9ea0d8b677a65e08b9cff495e5b25b693
tchfer <tchfernando@gmail.com>
2023-05-31 03:33:50 -0300

    Revert "Add home styling"
    This reverts commit c89e6328a51415b5084496ebbd9a06c1a22f8a6f.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './home.css';
-
+ 
 const Home: React.FC = () => {
-    return (
-      <div className="home-container">
-        <div className="content">
-          <div className="image-container">
-            <img src={require('./task-img.svg')} alt="Task Image" />
-          </div>
-          <div className="text-container">
-            <h1 className="home-title">Meu Gerenciador de Tarefas</h1>
-            <p className="home-description">Organize suas tarefas de forma simples e eficiente.</p>
-            <Link to="/tarefas" className="task-button">
-              Começar
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  };
-  
-  export default Home;
-  
+    return <h1>Página inicial</h1>
+}
+ 
+export default Home;
